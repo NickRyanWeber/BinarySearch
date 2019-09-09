a2d27e8f732b3165e6f40efa8e08094951b10820
nickryanweber <nicholasryanweber@gmail.com>
2019-09-09 14:40:39 -0400

    logic added and animation

--- Program.cs
+++ Program.cs
@@ -8,25 +8,113 @@ namespace BinarySearch
     {
       var lower = 0m;
       var upper = 100m;
+      var guess = Math.Floor((lower + upper) / 2);
       //   var tries = 0;
       var success = false;
 
+      Console.Write("Welcome to ");
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.Write("Number Guesser! ");
+      Console.ResetColor();
+      Console.WriteLine("The game where the computer guesses a number you pick.");
+      System.Threading.Thread.Sleep(2000);
+      Console.WriteLine("In order to play, you need to understand the rules...");
+      System.Threading.Thread.Sleep(1000);
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.Write("FIRST,");
+      Console.ResetColor();
+      Console.Write(" pick a number between ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.Write(lower);
+      Console.ResetColor();
+      Console.Write(" & ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine(upper);
+      Console.ResetColor();
+      System.Threading.Thread.Sleep(1000);
+      Console.Write("If your number is guessed, type ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("YES");
+      Console.ResetColor();
+      System.Threading.Thread.Sleep(1000);
+      Console.Write("If your number higher than the guess, type ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("HIGHER");
+      Console.ResetColor();
+      System.Threading.Thread.Sleep(1000);
+      Console.Write("If your number lower than the guess, type ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("LOWER");
+      Console.ResetColor();
+      System.Threading.Thread.Sleep(1000);
+      Console.Write("READY");
+      System.Threading.Thread.Sleep(250);
+      Console.Write(".");
+      System.Threading.Thread.Sleep(250);
+      Console.Write(".");
+      System.Threading.Thread.Sleep(250);
+      Console.Write(".");
+      System.Threading.Thread.Sleep(250);
+      Console.Write("?");
+      System.Threading.Thread.Sleep(250);
+      Console.Write("?");
+      System.Threading.Thread.Sleep(250);
+      Console.WriteLine("?");
+      Console.WriteLine("");
+
       while (!success)
       {
-        var guess = Math.Floor((lower + upper) / 2);
-        Console.WriteLine($"Is your number {guess}?");
+        Console.Write("Is your number ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{guess}?");
+        Console.ResetColor();
         var input = Console.ReadLine();
-        if (input == "y")
+        if (input.ToLower() == "y" || input.ToLower() == "yes")
         {
-          success = true;
+          Console.WriteLine("Do you want to play again? Y/N");
+          var input2 = Console.ReadLine();
+          if (input2.ToLower() == "y" || input2.ToLower() == "yes")
+          {
+            upper = 100;
+            lower = 0;
+          }
+          else if (input2.ToLower() == "n" || input2.ToLower() == "no")
+          {
+            success = true;
+          }
+          else
+          {
+            Console.WriteLine("Hmmm...I don't recognize that response, please try again");
+            Console.WriteLine("Do you want to play again? Y/N");
+          }
         }
-        else if (input == "higher")
+        else if (input.ToLower() == "higher" || input.ToLower() == "h")
         {
-          lower = guess;
+          if (guess == lower || guess == upper)
+          {
+            Console.WriteLine($"Don't lie, I know your number is {guess}");
+          }
+          else
+          {
+            lower = guess;
+            guess = Math.Ceiling((lower + upper) / 2);
+          }
         }
-        else if (input == "lower")
+        else if (input.ToLower() == "lower" || input.ToLower() == "l")
         {
-          upper = guess;
+          if (upper == guess || lower == guess)
+          {
+            Console.WriteLine($"Don't lie, I know your number is {guess}");
+          }
+          else
+          {
+            upper = guess;
+            guess = Math.Floor((lower + upper) / 2);
+          }
+        }
+        else
+        {
+          Console.WriteLine("Hmmm...I don't recognize that response, please try again");
         }
       }
     }
